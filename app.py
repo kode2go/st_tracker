7032d9a9f663b3557c65fbc52fe6c8749e8cc378
kode2go <brsbin001@myuct.ac.za>
2023-02-06 06:18:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,28 +19,30 @@ Tracking the parts test processing
 """
 QTY_of_failed = 0
 QTY_of_passed = 0
-QTY = int(st.text_input('Tracking the parts test processing:','100'))
+QTY = int(st.text_input('Please enter the quantity of parts:','100'))
 Lot_num = st.text_input('Please enter the lot number','qwe')
 OP = st.text_input('Please enter Operator name:','Sina')
 
-QTY_of_passed = st.text_input("Please enter the quantity of passed parts:  ",'90')
+st.write("Please start testing, Select 'Done' once you finish:")
+if st.button("Done"):
+    QTY_of_passed = st.text_input("Please enter the quantity of passed parts:  ",'90')
 
-QTY_of_failed = st.text_input("Please enter the quantity of failed parts:  ",'10')
+    QTY_of_failed = st.text_input("Please enter the quantity of failed parts:  ",'10')
+    
+    QP = int(QTY_of_passed)
+    QF = int(QTY_of_failed)
 
-QP = int(QTY_of_passed)
-QF = int(QTY_of_failed)
-
-if (QP + QF) == QTY:    
-    st.write(f"There are {QP} available parts to distributing ")
-    Distributed = int(st.text_input("Please enter how many parts are distributed?  ",'40'))
-    if Distributed <= QP:        
-        Balance1 = QP - Distributed
-        Balance = Balance1
-        print(f"You have {Balance1} parts left as balance ")
+    if (QP + QF) == QTY:    
+        st.write(f"There are {QP} available parts to distributing ")
+        Distributed = int(st.text_input("Please enter how many parts are distributed?  ",'40'))
+        if Distributed <= QP:        
+            Balance1 = QP - Distributed
+            Balance = Balance1
+            print(f"You have {Balance1} parts left as balance ")
 
 
-if st.button('Submit'):
-    db.put({"a_operator": OP,"b_Lot_num": Lot_num,"c_QTY": QTY,"d_QTY_of_passed": QTY_of_passed,"e_QTY_of_failed": QTY_of_failed,"f_Available": QTY_of_passed,"g_Balance": Balance,"h_time": timestampStr })
+    if st.button('Submit'):
+        db.put({"a_operator": OP,"b_Lot_num": Lot_num,"c_QTY": QTY,"d_QTY_of_passed": QTY_of_passed,"e_QTY_of_failed": QTY_of_failed,"f_Available": QTY_of_passed,"g_Balance": Balance,"h_time": timestampStr })
 
 #     new_row = pd.Series([timestampStr, chatbot_input, answer], index=df.columns)
 #     df = df.append(new_row,ignore_index=True) 
